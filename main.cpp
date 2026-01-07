2aa19392b608b621fe8ca9fb7902f5cc8fe66055
SquidBow <ggoodlike72@gmail.com>
2026-01-07 11:42:33 +0200

    Added placing and destroying "blocks" with left click. Added W A D and Space for movement

--- main.cpp
+++ main.cpp
@@ -42,14 +42,6 @@ void createWorld (array<array<int, 1000>, 1000>& world, Player& player) {
         }
     }
 
-    // world.at(player.posY/20).at(player.posX/20) = 2;
-    // world.at(player.posY/20 - 1).at(player.posX/20) = 2;
-    // world.at(player.posY/20 + 1).at(player.posX/20) = 2;
-
-    // world.at(player.posY/20).at(player.posX/20 + 1) = 2;
-    // world.at(player.posY/20 - 1).at(player.posX/20 + 1) = 2;
-    // world.at(player.posY/20 + 1).at(player.posX/20 + 1) = 2;
-
 
     world.at(player.posY/20).at(player.posX/20 + 3) = 1;
     world.at(player.posY/20 - 1).at(player.posX/20 + 3) = 1;
@@ -62,8 +54,6 @@ void createWorld (array<array<int, 1000>, 1000>& world, Player& player) {
 }
 
 void DrawWorld (const array<array<int, 1000>, 1000>& world, const Player& player, array<Color, 10>& blockColors) {
-    // int camX = player.posX;
-    // int camY = player.posY;
 
     //Player pos / 20 is the player grid position
     int playerGridX = player.posX / 20;
@@ -91,29 +81,13 @@ void DrawWorld (const array<array<int, 1000>, 1000>& world, const Player& player
             int camX = i * blockWidth - playerOffcetX;
             int camY = j * blockWidth - playerOffcetY;
 
-            // cout << world.at(worldY).at(worldX) << " ";
 
             DrawRectangle(camX, camY, blockWidth, blockWidth, blockColors.at(world.at(worldY).at(worldX)));
 
-            // if (world.at(worldY).at(worldX) == 1) {
-            //     DrawRectangle(camX, camY, blockWidth, blockWidth, BROWN);
-            // }
-            // // Actual grid player pos
-            // else if (world.at(worldY).at(worldX) == 2) {
-            //     DrawRectangle(camX, camY, blockWidth, blockWidth, ORANGE);
-            // }
         }
 
-        // DrawRectangle(screenWidht / 2, screenHeight / 2, 2, 2, GREEN);
-
-        // for (int j =  screenHeight / 2 - blockWidth * 1.5; j <  screenHeight / 2 - blockWidth * 1.5 + 55; j ++) {
-        //     DrawRectangle(screenWidht / 2 - blockWidth + 40, j, 1, 1, GREEN);
-        // }
-
-        // cout << "\nRow: " << j << "\n";
-
         //Draw player on top
-        DrawRectangle(screenWidht / 2 - blockWidth, screenHeight / 2 - (int)(blockWidth * 1.5 + 10), blockWidth * 2, blockWidth * 3, BLACK);
+        DrawRectangle(screenWidht / 2 - blockWidth, screenHeight / 2 - blockWidth * 1.5 + 10, blockWidth * 2, blockWidth * 3, BLACK);
     }
 }
 
@@ -142,61 +116,48 @@ int main () {
     float verticalSpeed = 0;
     bool jumped = false;
 
-    // short jumpFrameBuffer = 0;
-
     InitWindow(blocksInARow * blockWidth, blocksInACol * blockWidth, "Terraria clone");
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(blackgrowndColor);
+
         verticalSpeed += gravity;
-        // jumpFrameBuffer --;
-
-        // world.at(player.posY/20).at(player.posX/20) = 0;
-        // world.at(player.posY/20 - 1).at(player.posX/20) = 0;
-        // world.at(player.posY/20 + 1).at(player.posX/20) = 0;
-
-        // world.at(player.posY/20).at(player.posX/20 + 1) = 0;
-        // world.at(player.posY/20 - 1).at(player.posX/20 + 1) = 0;
-        // world.at(player.posY/20 + 1).at(player.posX/20 + 1) = 0;
 
         int predictMoveX = player.posX;
         int predictMoveY = player.posY;
-        // bool colidingX = false;
-        // bool colidingY = false;
 
-        if (IsKeyPressed(KEY_UP)) {
-            // if (jumped) {
-            //     jumpFrameBuffer = 5;
-            // }
-            // else {
+        if (IsKeyPressed(KEY_UP) || IsKeyPressed(KEY_W) || IsKeyPressed(KEY_SPACE)) {
                 verticalSpeed = -jumpHeight;
                 jumped = true;
-                // jumpFrameBuffer = 0;
-            // }
-            // lastDirection = 'u';
-            // predictMoveY -= playerSpeedMod;
         }
-        // else if (jumpFrameBuffer > 0 && !jumped) {
-        //     verticalSpeed = -jumpHeight;
-        //     jumped = true;
-        //     jumpFrameBuffer = 0;
-        // }
 
-        // if (IsKeyDown(KEY_DOWN)) {
-        //     lastDirection = 'd';
-        //     predictMoveY += playerSpeedMod;
-        // }
-        if (IsKeyDown(KEY_RIGHT)) {
+        if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) {
             lastDirection = 'r';
             predictMoveX += playerSpeedMod;
         }
-        if (IsKeyDown(KEY_LEFT)) {
+        if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) {
             lastDirection = 'l';
             predictMoveX -= playerSpeedMod;
         }
-        // if (GetKeyPressed() == 0) {
-        //     lastDirection = 'n';
-        // }
+
+        // Get mouse pos
+        int mouseGridX = ((GetMouseX() + player.posX - screenWidht / 2) + blockWidth) / 20;
+        int mouseGridY = ((GetMouseY() + player.posY - screenHeight / 2) + blockWidth) / 20;
+
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && IsCursorOnScreen()) {
+
+            // cout << "\nmouseGridX: " << mouseGridX << "\nmouseGridY: " << mouseGridY;
+            // cout << "\nplayerPosGridX: " << player.posX << "\nplayerPosGridY: " << player.posY << "\n";
+            if (world.at(mouseGridY).at(mouseGridX) != 0) {
+                world.at(mouseGridY).at(mouseGridX) = 0;
+            }
+            else {
+                world.at(mouseGridY).at(mouseGridX) = 1;
+            }
+        }
+
 
         if (verticalSpeed > 10) {
             verticalSpeed = 10;
@@ -212,20 +173,13 @@ int main () {
             //Check X colision
             //Check 4 blocks Y and 3 blocks X
             int predictMoveXRight = predictMoveX + 40;
-            // cout << "player.posY row: " << player.posY << endl;
-            // int predictMoveXGridRight = predictMoveXRight / 20;
-            // cout << "player.posY / 20 row: " << player.posY / 20 << endl;
             for (int j = player.posY; j < player.posY + blockWidth * 3; j ++) {
-                // cout << "Checking row: " << j << " col: " << predictMoveXGridRight << endl;
-
                 if (world.at(j / 20).at(predictMoveXRight / 20) != 0) predictMoveX = (predictMoveXRight / 20) * 20 - 40;
             }
         }
-
         if (predictMoveX < player.posX) {
             //Look left
             for (int j = player.posY; j < player.posY + blockWidth * 3; j ++) {
-                // cout << "Checking row: " << j << " col: " << predictMoveXGridRight << endl;
                 if (world.at(j / 20).at(predictMoveX / 20) != 0) predictMoveX = (predictMoveX / 20) * 20 + 20;
             }
         }
@@ -242,49 +196,16 @@ int main () {
         }
         if (predictMoveY < player.posY) {
             //Look up
-
             // 3 blocks. below the player, 1 right and 2 right
             for (int j = player.posX; j < player.posX + blockWidth * 2; j ++) {
                 if (world.at(predictMoveY / 20).at(j / 20) != 0) predictMoveY = (predictMoveY / 20) * 20 + 20;
             }
         }
 
-
-        // for (int j = player.posX / 20; j < player.posX / 20 + 2; j ++) {
-        //     int predictMoveYGridDown = predictMoveY / 20 + 2;
-        //     if (world.at(j).at(predictMoveYGridDown) != 0) predictMoveY = predictMoveYGridDown * 20 - 60;
-        // }
-
         player.posX = predictMoveX;
         player.posY = predictMoveY;
 
-        //Check X colision
-        //Check 4 blocks Y and 3 blocks X
-        // for (int i = predictMoveY; i < predictMoveX + 3; i ++) {
-        //     for (int j = predictMoveX; j < predictMoveX + 2; j ++) {
-        //         if (world.at(i).at(j) != 0) predictMoveX = player.posX;
-        //     }
-        // }
-
-        // if (world.at(predictMoveY / 20).at(predictMoveX / 20) || world.at(predictMoveY / 20).at(predictMoveX / 20 + 1) || world.at(predictMoveY / 20).at(predictMoveX / 20 + 2)) {
-
-        // }
-        // world.at(player.posY/20).at(player.posX/20) = 2;
-        // world.at(player.posY/20 - 1).at(player.posX/20) = 2;
-        // world.at(player.posY/20 + 1).at(player.posX/20) = 2;
-
-        // world.at(player.posY/20).at(player.posX/20 + 1) = 2;
-        // world.at(player.posY/20 - 1).at(player.posX/20 + 1) = 2;
-        // world.at(player.posY/20 + 1).at(player.posX/20 + 1) = 2;
-
-        BeginDrawing();
-        ClearBackground(blackgrowndColor);
-
         DrawWorld(world, player, blockColors);
-
-        // for (int j = player.posY / 20; j < player.posY / 20 + 3; j ++) {
-        //     DrawObjectWithPlayerOffcet(predictMoveXGridRight, j, 1, 20, GREEN, player);
-        // }
 
         EndDrawing();
     }
